1c53e9103621932e7734ea938eced10cc47fc1ae
user <user@user8>
2021-12-13 18:17:55 +0900

    버전 2.0 변경내용 : 로그인 기능 업그레이드 작성자 : 김현진

--- test826java/src/main/java/test826java/TestJava.java
+++ test826java/src/main/java/test826java/TestJava.java
@@ -3,6 +3,7 @@ package test826java;
 public class TestJava {
 	public static void main(String[] args) {
 		System.out.println("Hello Git World!");
-		System.out.println("Version 1.0");
+		System.out.println("Version 2.0");
+		System.out.println("로그인 됨!");
 	}
 }
