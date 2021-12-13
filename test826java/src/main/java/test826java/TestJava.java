944bc85e1e1612be7dbab26b46ce04463021e977
user <user@user8>
2021-12-13 19:23:01 +0900

    버전 4.0 변경점 : TestJava2 변경

--- test826java/src/main/java/test826java/TestJava.java
+++ test826java/src/main/java/test826java/TestJava.java
@@ -3,7 +3,7 @@ package test826java;
 public class TestJava {
 	public static void main(String[] args) {
 		System.out.println("Hello Git World!");
-		System.out.println("Version 3.0");
+		System.out.println("Version 4.0");
 		System.out.println("인코딩 변경 로그인 기능 업그레이드");
 	}
 }
--- test826java/src/main/java/test826java/TestJava2.java
+++ test826java/src/main/java/test826java/TestJava2.java
@@ -3,5 +3,6 @@ package test826java;
 public class TestJava2 {
 	public static void main(String[] args) {
 		System.out.println("새로운 Java 코드");
+		System.out.println("새로운 Java 코드 변경부 2.0");
 	}
 }
