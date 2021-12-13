fa37281cc347017075c625210c2dd1eb28419c89
user <user@user8>
2021-12-13 19:26:51 +0900

    버전 6.0

--- test826java/src/main/java/test826java/TestJava.java
+++ test826java/src/main/java/test826java/TestJava.java
@@ -5,5 +5,6 @@ public class TestJava {
 		System.out.println("Hello Git World!");
 		System.out.println("Version 5.0");
 		System.out.println("인코딩 변경 로그인 기능 업그레이드");
+		System.out.println("보기 기능 추가!");
 	}
 }
