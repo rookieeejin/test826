b03c091d5ebc82b571c0a303f448ed5bf285c0dc
user <user@user8>
2021-12-22 21:53:57 +0900

    test기능

--- test826java/src/main/java/test826java/TestJava.java
+++ test826java/src/main/java/test826java/TestJava.java
@@ -6,5 +6,6 @@ public class TestJava {
 		System.out.println("Version 5.0");
 		System.out.println("인코딩 변경 로그인 기능 업그레이드");
 		System.out.println("보기 기능 추가!");
+		System.out.println("테스트 한번더");
 	}
 }
